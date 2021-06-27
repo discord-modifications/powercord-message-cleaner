84bf793b39dc8952c285a64d9b68ae4f870b4d9f
eternal <73202350+slow@users.noreply.github.com>
2021-06-27 23:11:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -296,10 +296,10 @@ module.exports = class MessageCleaner extends Plugin {
    }
 
    random(length) {
-      var result = '';
-      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var charactersLength = characters.length;
-      for (var i = 0; i < length; i++) {
+      let result = '';
+      let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
          result += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
       return result;
